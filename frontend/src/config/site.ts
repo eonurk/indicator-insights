9ae631385234b4a7b052c913f4b99bd5681c9fba
eonurk <onurkara3594@gmail.com>
2024-08-26 18:02:57 +0200

    twitter

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
 	description:
 		"Indicator Insights is the best platform for actionable insights from financial indicators. Unlock powerful tools to simplify data, track trends, and make winning investment decisions.",
 	links: {
-		twitter: "https://twitter.com/eonurkara",
+		twitter: "https://twitter.com/helpindicator",
 		github: "https://github.com/eonurk/indicator-insights",
 		discord: "https://discord.gg/f6JeCy57bH",
 	},
