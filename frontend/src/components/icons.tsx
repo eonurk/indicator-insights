0d58a80c5aa1d1411268e2e07959a436bb94f6c9
eonurk <onurkara3594@gmail.com>
2024-09-04 12:31:26 +0200

    home icons to icons.tsx

--- frontend/src/components/icons.tsx
+++ frontend/src/components/icons.tsx
@@ -11,6 +11,18 @@ export const Icons = {
 			{...props}
 		/>
 	), // Return an img element
+
+	telegram: (props: IconProps) => (
+		<svg
+			{...props}
+			viewBox="0 0 496 512"
+			width="36"
+			height="36"
+			fill="rgb(37, 99, 235)"
+		>
+			<path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z" />
+		</svg>
+	),
 	twitter: (props: IconProps) => (
 		<svg
 			{...props}
@@ -125,6 +137,25 @@ export const Icons = {
 			{...props}
 		>
 			<path d="M21 12a9 9 0 1 1-6.219-8.56" />
+		</svg>
+	),
+
+	info: (props: IconProps) => (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="16"
+			height="16"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<circle cx="12" cy="12" r="10" />
+			<line x1="12" y1="16" x2="12" y2="12" />
+			<line x1="12" y1="8" x2="12.01" y2="8" />
 		</svg>
 	),
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -25,6 +25,7 @@ import {
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { Icons } from "@/components/icons";
 
 <script async src="https://js.stripe.com/v3/pricing-table.js"></script>;
 
@@ -149,15 +150,8 @@ export function Home({ user }: StockChartProps) {
 						</Select>
 						<p className="text-xs text-white opacity-80 text-center">
 							<br />
-							<span className="inline-flex items-center">
-								<svg
-									className="w-4 h-4 text-slate-500 z-0"
-									fill="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path d="M12 2L2 22h20L12 2z" />
-								</svg>
+							<span className="inline-flex items-center gap-2">
+								<Icons.info />
 								<span className="text-white text-xs opacity-80 ">
 									Only the stocks of the selected index are shown. It might take
 									a few seconds to load.
@@ -291,15 +285,7 @@ export function Home({ user }: StockChartProps) {
 								variant="outline"
 								className="text-slate-700 transition-colors py-8 px-8 text-base shadow-md flex items-center space-x-4 rounded-lg"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 496 512"
-									width="36"
-									height="36"
-									fill="rgb(37, 99, 235)"
-								>
-									<path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z" />
-								</svg>
+								<Icons.telegram />
 								<span className="sm:inline hidden">
 									Join our Telegram for Free Daily Insights
 								</span>
